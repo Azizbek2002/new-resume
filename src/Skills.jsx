b6c8b702a9b26b911005b23f5d933df1a350fd31
Azizbek2002 <azimovazizbek12.02.02@gmail.com>
2023-03-31 18:09:53 -0700

    Styled skills section

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,32 +1,40 @@
 import styles from "./Skills.module.css";
+import html from "./assets/images/html.png";
+import css3 from "./assets/images/css3.png";
+import js from "./assets/images/js.png";
+import reactIcon from "./assets/images/react.png";
+import redux from "./assets/images/redux.png";
+import github from "./assets/images/github.png";
 
 const Skills = () => {
   return (
     <div className={styles.Skills_section}>
-      <div className={styles.skills}>
-        <div className={styles.skill}>
-          <h1 className={styles.skill_title}>HTML</h1>
-          <img src="" alt="Html" />
-        </div>
-        <div className={styles.skill}>
-          <h1 className={styles.skill_title}>Css</h1>
-          <img src="" alt="Css" />
-        </div>
-        <div className={styles.skill}>
-          <h1 className={styles.skill_title}>JavaScript</h1>
-          <img src="" alt="JavaScript" />
-        </div>
-        <div className={styles.skill}>
-          <h1 className={styles.skill_title}>React.js</h1>
-          <img src="" alt="React.js" />
-        </div>
-        <div className={styles.skill}>
-          <h1 className={styles.skill_title}>React-router-dom</h1>
-          <img src="" alt="React-router-dom" />
-        </div>
-        <div className={styles.skill}>
-          <h1 className={styles.skill_title}>Github</h1>
-          <img src="" alt="github" />
+      <div className={styles.container}>
+        <div className={styles.skills}>
+          <div className={styles.skill}>
+            <h1 className={styles.skill_title}>HTML</h1>
+            <img src={html} alt="Html" />
+          </div>
+          <div className={styles.skill}>
+            <h1 className={styles.skill_title}>Css</h1>
+            <img src={css3} alt="Css" />
+          </div>
+          <div className={styles.skill}>
+            <h1 className={styles.skill_title}>JavaScript</h1>
+            <img src={js} alt="JavaScript" />
+          </div>
+          <div className={styles.skill}>
+            <h1 className={styles.skill_title}>React.js</h1>
+            <img src={reactIcon} alt="React.js" />
+          </div>
+          <div className={styles.skill}>
+            <h1 className={styles.skill_title}>React-router-dom</h1>
+            <img src={redux} alt="redux" />
+          </div>
+          <div className={styles.skill}>
+            <h1 className={styles.skill_title}>Github</h1>
+            <img src={github} alt="github" />
+          </div>
         </div>
       </div>
     </div>
